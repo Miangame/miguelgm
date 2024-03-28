9b4982bd8045262d03d5d27790c23cef6bbfb7f9
Miguel Gavilán <miangame1@gmail.com>
2024-03-28 20:44:19 +0100

    feat: prepare repo

---
+++ src/app/_components/layout/Layout.styled.ts
@@ -0,0 +1,13 @@
+'use client'
+
+import styled from 'styled-components'
+
+export const LayoutWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`
+
+export const LayoutContent = styled.div`
+  flex: 1;
+`
---
+++ src/style/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    black: '#151515',
+    white: '#fff',
+    red: '#e4381C',
+    orange: '#e16521',
+    success: '#00a86b'
+  }
+}
