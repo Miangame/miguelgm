891199a4764d90e14840a52493349781f76464b7
Miguel Gavilán <miangame1@gmail.com>
2024-11-30 09:46:50 +0100

    feat: add number of years

--- src/components/Home/components/Presentation/Presentation.tsx
+++ src/components/Home/components/Presentation/Presentation.tsx
@@ -13,15 +13,42 @@ import {
 } from './Presentation.styled'
 import { SOCIAL } from '@/constants/social'
 
+const calculateYears = (date: string) => {
+  const currentDate = new Date()
+  const newDate = new Date(date)
+
+  let years = currentDate.getFullYear() - newDate.getFullYear()
+
+  const currentMonth = currentDate.getMonth()
+  const currentDay = currentDate.getDate()
+  const newMonth = newDate.getMonth()
+  const newDay = newDate.getDate()
+
+  if (
+    currentMonth < newMonth ||
+    (currentMonth === newMonth && currentDay < newDay)
+  ) {
+    years--
+  }
+
+  return years
+}
+
 const Presentation = () => {
   const { t } = useTranslation('presentation')
+
+  const numberOfYears = calculateYears('2018-05-01')
 
   return (
     <Section id="presentation" $hasPaddings>
       <Wrapper>
         <StyledImage src="/img/profile.webp" />
         <StyledTitle>{t('title')}</StyledTitle>
-        <Description dangerouslySetInnerHTML={{ __html: t('description') }} />
+        <Description
+          dangerouslySetInnerHTML={{
+            __html: t('description', { years: numberOfYears })
+          }}
+        />
 
         <SocialWrapper>
           {SOCIAL.map(({ link, icon: Icon, title }) => (
